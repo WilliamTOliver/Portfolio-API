c3796c24dd47625d073f7da3c88cf626266ce9ec
WilliamTOliver <WilliamTOliver@outlook.com>
2018-11-25 16:35:10 -0600

    add direct check auth call and secure chart endpoints

--- adapter/controllers/auth.js
+++ adapter/controllers/auth.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const AuthService = require('../services/auth');
 const checkAuth = require('../middleware/check-auth');
 
+// Direct Check Auth
+router.get('/', checkAuth, (req, res) => {
+  res.status(200).json({message: 'Authorized', userDetails: req.userData})
+});
+
 router.post('/signup', AuthService.signup);
 
 router.post('/login', AuthService.login);
--- adapter/controllers/chart.js
+++ adapter/controllers/chart.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
 
 const ChartService = require('../services/chart');
 
-router.get('/build', ChartService.buildCharts);
-router.get('/', ChartService.getCharts);
+router.get('/build', checkAuth, ChartService.buildCharts);
+router.get('/', checkAuth, ChartService.getCharts);
 
 
 module.exports = router;
