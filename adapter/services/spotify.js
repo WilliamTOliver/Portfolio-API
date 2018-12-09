2f566399cf1ae20461aac9e9e298cc3b1899d30a
WilliamTOliver <WilliamTOliver@outlook.com>
2018-12-08 20:16:28 -0600

    wip import playlists

--- adapter/services/spotify.js
+++ adapter/services/spotify.js
@@ -1,14 +1,10 @@
 var SpotifyWebApi = require('spotify-web-api-node');
 
-
+function ()
 
 exports.requestToken = reqbody => {
   // credentials are optional
-  var spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT,
-    clientSecret: process.env.SPOTIFY_SECRET,
-    redirectUri: process.env.SPOTIFY_REDIRECT
-  });
+
   return spotifyApi.authorizationCodeGrant(reqbody.code).then(
     data => {
       // Set the access token and refresh token
