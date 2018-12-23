27a3b96bd1243c09e3883b24f48ed7d971dab1b5
WilliamTOliver <WilliamTOliver@outlook.com>
2018-12-23 16:05:22 -0600

    accept custom spotify auth header

--- adapter/controllers/spotify.js
+++ adapter/controllers/spotify.js
@@ -4,19 +4,19 @@ const router = express.Router();
 const SpotifyService = require('../services/spotify');
 const checkAuth = require('../middleware/check-auth');
 const responseHelper = require('./../services/helpers/responseHelper');
-
+const SpotifyAuthHeader = 'X-Spotify-Auth';
 router.post('/token', checkAuth, (req, res) => {
   SpotifyService.requestToken(req.body)
     .then(data => responseHelper.success(res, 200, data))
     .catch(err => responseHelper.error());
 });
-router.get('/user/:token', checkAuth, (req, res) => {
-  SpotifyService.getUserInfo(req.params.token)
+router.get('/user', checkAuth, (req, res) => {
+  SpotifyService.getUserInfo(req.get(SpotifyAuthHeader))
     .then(data => responseHelper.success(res, 200, data))
     .catch(err => responseHelper.error());
 });
-router.get('/playlist/:token', checkAuth, (req, res) => {
-  SpotifyService.getUserPlaylists(req.params.token)
+router.get('/user/playlists', checkAuth, (req, res) => {
+  SpotifyService.getUserPlaylists(req.get(SpotifyAuthHeader))
     .then(data => responseHelper.success(res, 200, data))
     .catch(err => responseHelper.error());
 });
